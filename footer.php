14ae0650cbbd95f998552c370b5bee2e1b6e960e
aishaali3 <113482703+aishaali3@users.noreply.github.com>
2025-07-21 01:01:54 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+  <p>&copy; 2025 Ali Hadi. All rights reserved.</p>
+</footer>
+
+<?php wp_footer(); ?>
+
+<script>
+  function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+  }
+</script>
+
+</body>
+</html>
